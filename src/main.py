0262c4c8131b50359a56ff8c0dbf517184b5046c
ocapraro <25327166+ocapraro@users.noreply.github.com>
2026-03-03 14:13:25 -0500

    add encoding bytes to an inventory

--- src/main.py
+++ src/main.py
@@ -13,7 +13,6 @@ def encode_item(item:Item):
 
 # given a full encoded inventory, figure out the different arrangements and return them as encoded items
 def calculate_inventory_arrangements(inventory:list[str]):
-  # return set(permutations(inventory))
   return unique_permutation_count(inventory)
 
 # given a stack size, and number of slots to spread it to, return all the possible combinations (order ignored)
@@ -44,7 +43,74 @@ def calculate_inventories(items:list[Item]):
         ))
   return inventories
 
+def get_inventory_counts(items:list[Item], verbose=False):
+  # The different combinations of items
+  encoded_inventories = calculate_inventories(items)
+  inventory_counts = {}
+  abstract_inventories = {}
+
+  # For each inventory, calculate the number of permutations for it
+  for i,encoded_inventory in enumerate(encoded_inventories):
+    if verbose:
+      print(f"Checking Inventory {i+1}/{len(encoded_inventories)}")
+    inventory = encoded_inventory.split(",")
+    abstract_inventory_dict = {}
+    for item in inventory:
+      if item not in abstract_inventory_dict.keys():
+        abstract_inventory_dict[item] = 0
+      abstract_inventory_dict[item] += 1
+    abstract_inventory = ",".join(str(v) for v in [sorted(abstract_inventory_dict.values())])
+    if abstract_inventory in abstract_inventories.keys():
+      i_count = abstract_inventories[abstract_inventory]
+    else:
+      i_count = calculate_inventory_arrangements(inventory)
+      abstract_inventories[abstract_inventory] = i_count
+    if verbose:
+      print(f"Found {i_count} permutations")
+    inventory_counts[encoded_inventory] = i_count
+  return inventory_counts
+
+
+def get_inventory(number:int, items:list[Item], verbose=False):
+  # Get all the inventory counts
+  inventory_counts = get_inventory_counts(items)
+  count = 0
+  encoded_inventory = None
+  # For each inventory, see how many permutations it has until you have overshot the target inventory
+  for key in inventory_counts.keys():
+    cur_count = inventory_counts[key]
+    if count+cur_count>=number:
+      encoded_inventory = str(key)
+      break
+    count += cur_count
+  if not encoded_inventory:
+    if verbose:
+      print("No valid inventory found")
+    return []
   
+  inventory_combination = encoded_inventory.split(",")
+  if verbose:
+    print(f"Found inventory combination: {inventory_combination}")
+  perm_number = number - count
+  final_inventory = []
+  perms_to_check = [i for i in inventory_combination]
+  count = 0
+  while len(perms_to_check) > 0:
+    checked_items = set()
+    for item in perms_to_check:
+      if item in checked_items:
+        continue
+      checked_items.add(item)
+      remaining_inventory = [i for i in perms_to_check]
+      remaining_inventory.remove(item)
+      perm_count = unique_permutation_count(remaining_inventory)
+      if count+perm_count >= perm_number:
+        final_inventory.append(item)
+        perms_to_check.remove(item)
+        break
+      count += perm_count
+  return final_inventory
+
 
 items:list[Item] = [
   {
@@ -103,25 +169,7 @@ items:list[Item] = [
   }
 ]
 
-count = 0
-inventories = calculate_inventories(items)
-abstract_inventories = {}
-for ii,i in enumerate(inventories):
-  print(f"Checking Inventory {ii+1}/{len(inventories)}")
-  inventory = i.split(",")
-  abstract_inventory_dict = {}
-  for item in inventory:
-    if item not in abstract_inventory_dict.keys():
-      abstract_inventory_dict[item] = 0
-    abstract_inventory_dict[item] += 1
-  abstract_inventory = ",".join(str(v) for v in [sorted(abstract_inventory_dict.values())])
-  if abstract_inventory in abstract_inventories.keys():
-    i_count = abstract_inventories[abstract_inventory]
-  else:
-    # i_count = len(calculate_inventory_arrangements(inventory))
-    i_count = calculate_inventory_arrangements(inventory)
-    abstract_inventories[abstract_inventory] = i_count
-  print(f"Found {i_count} permutations")
-  count += i_count
 
-print(f"Total: {count}")
+
+print(f"Total: {sum(get_inventory_counts(items).values())}")
+print("\n".join(get_inventory(10000, items)))
