9de3c2279d4c0352365d8643ef7d2d504357d4c4
ocapraro <25327166+ocapraro@users.noreply.github.com>
2026-03-04 00:30:29 -0500

    add in encoding and decoding

--- src/main.py
+++ src/main.py
@@ -11,6 +11,16 @@ class Item(TypedDict):
 def encode_item(item:Item):
   return f"{item['name']}//{item['quantity']}//{item.get("unstackable", False)}"
 
+def decode_item(string:str)->Item:
+  parts = string.split("//")
+  item:Item =  {
+    "name": parts[0],
+    "quantity": int(parts[1]),
+  }
+  if len(parts) > 2:
+    item["unstackable"] = parts[2]=="True"
+  return item
+
 # given a full encoded inventory, figure out the different arrangements and return them as encoded items
 def calculate_inventory_arrangements(inventory:list[str]):
   return unique_permutation_count(inventory)
@@ -36,11 +46,11 @@ def calculate_inventories(items:list[Item]):
       stack = item["quantity"]
       spreads = spread_stack(stack, slots)
       for spread in spreads:
-        inventories.add(",".join(
+        inventories.add(",".join(sorted(
           [encode_item(spread_item) for spread_item in de_blanked if (spread_item["name"] != item["name"])] +
-          [f"{item['name']}//{spread_count}" for spread_count in spread]+
+          [f"{item['name']}//{spread_count}//False" for spread_count in spread]+
           ["Blank//1//True" for _ in range(blanks-i-1)]
-        ))
+        )))
   return inventories
 
 def get_inventory_counts(items:list[Item], verbose=False):
@@ -71,7 +81,7 @@ def get_inventory_counts(items:list[Item], verbose=False):
   return inventory_counts
 
 
-def get_inventory(number:int, items:list[Item], verbose=False):
+def get_inventory(number:int, items:list[Item], verbose=False) -> list[Item]:
   # Get all the inventory counts
   inventory_counts = get_inventory_counts(items)
   count = 0
@@ -91,10 +101,8 @@ def get_inventory(number:int, items:list[Item], verbose=False):
   inventory_combination = encoded_inventory.split(",")
   if verbose:
     print(f"Found inventory combination: {inventory_combination}")
-  perm_number = number - count
   final_inventory = []
   perms_to_check = [i for i in inventory_combination]
-  count = 0
   while len(perms_to_check) > 0:
     checked_items = set()
     for item in perms_to_check:
@@ -104,12 +112,42 @@ def get_inventory(number:int, items:list[Item], verbose=False):
       remaining_inventory = [i for i in perms_to_check]
       remaining_inventory.remove(item)
       perm_count = unique_permutation_count(remaining_inventory)
-      if count+perm_count >= perm_number:
-        final_inventory.append(item)
+      if count+perm_count >= number:
+        final_inventory.append(decode_item(item))
         perms_to_check.remove(item)
         break
       count += perm_count
   return final_inventory
+
+def get_inventory_index(inventory:list[Item], items:list[Item]):
+  sorted_inventory = sorted([encode_item(i) for i in inventory])
+  encoded_sorted_inventory = ",".join(sorted_inventory)
+  inventory_counts = get_inventory_counts(items)
+  if encoded_sorted_inventory not in inventory_counts.keys():
+    return -1
+  count = 1
+  for key in inventory_counts:
+    if key == encoded_sorted_inventory:
+      break
+    count += inventory_counts[key]
+
+  perms_to_check = [i for i in sorted_inventory]
+  for target_item in [encode_item(i) for i in inventory]:
+    checked_items = set()
+    for item in perms_to_check:
+      if item in checked_items:
+        continue
+      checked_items.add(item)
+      remaining_inventory = [i for i in perms_to_check]
+      remaining_inventory.remove(item)
+      perm_count = unique_permutation_count(remaining_inventory)
+      if item == target_item:
+        perms_to_check.remove(item)
+        break
+      count += perm_count
+  return count
+  
+
 
 def text_to_number(message:str):
   binary_message = [f"{ord(c):0{8}b}" for c in message]
@@ -182,6 +220,22 @@ items:list[Item] = [
 ]
 
 
+# 5157250560
+print(f"Total: {sum(get_inventory_counts(items).values())}")
+print(f"Accuracy: {sum(get_inventory_counts(items).values())/51572505.6}%")
+# print(get_inventory(text_to_number("Hi!"),items))
 
-# print(f"Total: {bin(sum(get_inventory_counts(items).values()))}")
-print(get_inventory(text_to_number("Hi!"),items))
+# Encode
+msg = "Lily"
+number_msg = text_to_number(msg)
+inventory = get_inventory(number_msg,items)
+
+inventory_index = get_inventory_index(inventory,items)
+decoded_msg = number_to_text(inventory_index)
+
+print(f"Encrypting Message: {msg}")
+print(f"Convert to binary: {number_msg}")
+print(f"Found inventory:\n - {'\n - '.join([str(item['quantity'])+item['name'] for item in inventory])}")
+
+print(f"Inventory Index: {inventory_index}")
+print(f"Decoded message: {decoded_msg}")
