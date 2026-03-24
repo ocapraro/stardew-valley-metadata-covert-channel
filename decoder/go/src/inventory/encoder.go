d3536ace8a61c2567e7eb5df7b65f0360baa8332
ocapraro <25327166+ocapraro@users.noreply.github.com>
2026-03-24 13:27:49 -0400

    add server

--- decoder/go/src/inventory/encoder.go
+++ decoder/go/src/inventory/encoder.go
@@ -1,7 +1,9 @@
 package inventory
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"strconv"
 	"strings"
 )
@@ -14,4 +16,83 @@ func TextToNumber(message string) uint64 {
 	}
 	binaryMessage, _ := strconv.ParseUint(binaryString.String(), 2, 64)
 	return binaryMessage
+}
+
+type Message struct {
+	Message string `json:"message"`
+}
+
+func StartEncoder() {
+	currentInventory := Inventory{
+		Items: []Item{
+			{
+				Name:  "Axe",
+				Stack: 1,
+			},
+			{
+				Name:  "Hoe",
+				Stack: 1,
+			},
+			{
+				Name:  "WateringCan",
+				Stack: 1,
+			},
+			{
+				Name:  "Pickaxe",
+				Stack: 1,
+			},
+			{
+				Name:  "Parsnip Seeds",
+				Stack: 10,
+			},
+			{
+				Name:  "MeleeWeapon",
+				Stack: 1,
+			},
+			{
+				Name:  "Parsnip Seeds",
+				Stack: 5,
+			},
+			{
+				Name:  "Blank",
+				Stack: 0,
+			},
+			{
+				Name:  "Blank",
+				Stack: 0,
+			},
+			{
+				Name:  "Blank",
+				Stack: 0,
+			},
+			{
+				Name:  "Blank",
+				Stack: 0,
+			},
+			{
+				Name:  "Blank",
+				Stack: 0,
+			},
+		},
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(Message{Message: "ok"})
+	})
+
+	mux.HandleFunc("GET /encode/{message}", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		message := r.PathValue("message")
+		numberMessage := TextToNumber(message)
+		variation := currentInventory.GetVariation(numberMessage)
+		json.NewEncoder(w).Encode(variation)
+	})
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	print(server.ListenAndServe())
 }
--- decoder/go/src/main.go
+++ decoder/go/src/main.go
@@ -5,63 +5,64 @@ import (
 )
 
 func main() {
-	currentInventory := inventory.Inventory{
-		Items: []inventory.Item{
-			{
-				Name:  "Axe",
-				Stack: 1,
-			},
-			{
-				Name:  "Hoe",
-				Stack: 1,
-			},
-			{
-				Name:  "WateringCan",
-				Stack: 1,
-			},
-			{
-				Name:  "Pickaxe",
-				Stack: 1,
-			},
-			{
-				Name:  "Parsnip Seeds",
-				Stack: 10,
-			},
-			{
-				Name:  "MeleeWeapon",
-				Stack: 1,
-			},
-			{
-				Name:  "Parsnip Seeds",
-				Stack: 5,
-			},
-			{
-				Name:  "Blank",
-				Stack: 0,
-			},
-			{
-				Name:  "Blank",
-				Stack: 0,
-			},
-			{
-				Name:  "Blank",
-				Stack: 0,
-			},
-			{
-				Name:  "Blank",
-				Stack: 0,
-			},
-			{
-				Name:  "Blank",
-				Stack: 0,
-			},
-		},
-	}
+	// currentInventory := inventory.Inventory{
+	// 	Items: []inventory.Item{
+	// 		{
+	// 			Name:  "Axe",
+	// 			Stack: 1,
+	// 		},
+	// 		{
+	// 			Name:  "Hoe",
+	// 			Stack: 1,
+	// 		},
+	// 		{
+	// 			Name:  "WateringCan",
+	// 			Stack: 1,
+	// 		},
+	// 		{
+	// 			Name:  "Pickaxe",
+	// 			Stack: 1,
+	// 		},
+	// 		{
+	// 			Name:  "Parsnip Seeds",
+	// 			Stack: 10,
+	// 		},
+	// 		{
+	// 			Name:  "MeleeWeapon",
+	// 			Stack: 1,
+	// 		},
+	// 		{
+	// 			Name:  "Parsnip Seeds",
+	// 			Stack: 5,
+	// 		},
+	// 		{
+	// 			Name:  "Blank",
+	// 			Stack: 0,
+	// 		},
+	// 		{
+	// 			Name:  "Blank",
+	// 			Stack: 0,
+	// 		},
+	// 		{
+	// 			Name:  "Blank",
+	// 			Stack: 0,
+	// 		},
+	// 		{
+	// 			Name:  "Blank",
+	// 			Stack: 0,
+	// 		},
+	// 		{
+	// 			Name:  "Blank",
+	// 			Stack: 0,
+	// 		},
+	// 	},
+	// }
 
-	msg := "Lily"
-	numberMsg := inventory.TextToNumber(msg)
-	println(len(currentInventory.Items))
-	variation := currentInventory.GetVariation(numberMsg)
-	variation.Print()
-	println(len(variation.Items))
+	// msg := "Lily"
+	// numberMsg := inventory.TextToNumber(msg)
+	// println(len(currentInventory.Items))
+	// variation := currentInventory.GetVariation(numberMsg)
+	// variation.Print()
+	// println(len(variation.Items))
+	inventory.StartEncoder()
 }
